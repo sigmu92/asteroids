4f94a0a7be0b08af9de4c6d7abdb2643693dcc6b
sigmu92 <maxwellw92@gmail.com>
2025-07-04 06:47:34 -0500

    add container functionality to player and main

--- main.py
+++ main.py
@@ -10,21 +10,28 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+        
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
 
 
+        updatable.update(dt)
 
-        
-        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip() 
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from circleshape import CircleShape
 from constants import *
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -25,6 +26,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt    
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -33,3 +38,10 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        
